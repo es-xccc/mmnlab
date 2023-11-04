25f7a0cf6539c7a5189378432ded98ede8f6de20
es-xccc <r2605572@gmail.com>
2023-11-05 00:06:50 +0800

    完成簡易登入註冊系統

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from polls import views as polls_views
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path('', polls_views.login_view, name='home'),
+    path('login_success/', polls_views.login_success, name='login_success'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 
-# Create your views here.
-from django.http import HttpResponse
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('login_success')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+        else:
+            return render(request, 'register.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def login_success(request):
+    return render(request, 'login_success.html')
