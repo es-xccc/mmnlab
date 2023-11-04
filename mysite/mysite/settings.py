caa98dcb774c8302828ec5487b07be2d7ff558a6
es-xccc <r2605572@gmail.com>
2023-11-04 19:40:58 +0800

    1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.mysql', # 或者使用 mysql.connector.django
+ 'NAME': 'test',
+ 'USER': 'test',
+ 'PASSWORD': 'test123',
+ 'HOST':'localhost',
+ 'PORT':'3306',
+ }
 }
 
 
