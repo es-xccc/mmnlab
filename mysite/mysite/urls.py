4a519dcbdfe7a3464e3c37e0a1b4879abe3774de
es-xccc <r2605572@gmail.com>
2023-11-07 10:01:17 +0800

    bootstrap

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,6 +5,5 @@ from polls import views as polls_views
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    path('', polls_views.login_view, name='home'),
-    path('login_success/', polls_views.login_success, name='login_success'),
+    path('', polls_views.redhom, name='home'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
+def redhom(request):
+    return redirect('login')
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -10,7 +12,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect('login_success')
+            return HttpResponseRedirect('/polls/login_success')
         else:
             return render(request, 'login.html', {'error': 'Invalid username or password'})
     else:
@@ -21,7 +23,6 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
             return redirect('login')
         else:
             return render(request, 'register.html', {'form': form})
@@ -31,3 +32,7 @@ def register(request):
 
 def login_success(request):
     return render(request, 'login_success.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/polls/login')
